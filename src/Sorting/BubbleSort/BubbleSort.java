6b62921d21f5cc68219a5e122b0cc1de177c4331
Mayank <mayanksonwani078@gmail.com>
2025-07-21 21:42:32 +0530

    Added few Searching questions

---
+++ src/Sorting/BubbleSort/BubbleSort.java
@@ -0,0 +1,30 @@
+package BubbleSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+       // int arr [] = {2,5,6,9,1,3};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void bubbleSort(int [] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
